155ad5e270aaf73763fe7b01afa8d35cb643ec67
jainarchi <imarchijain2405@gmail.com>
2026-04-15 22:40:50 +0530

    hook layer and state layer of interview

---
+++ Frontend/src/features/interview/hooks/useInterview.jsx
@@ -0,0 +1,78 @@
+import {generateInterviewReport , getAllInterviewReports , getInterviewReport , deleteInterviewReport} from '../services/interview.api.js'
+import { useContext } from 'react'
+import { InterviewContext } from '../interviewContext.jsx'
+
+
+
+export const useInterview = () => {
+    const {loading , setLoading , report , setReport , reports , setReports} = useContext(InterviewContext)
+
+   
+    const handleGenerateReport = async ({jobDescription , setDescription , resumeFile}) => {
+        try {
+            setLoading(true)
+            const data = await generateInterviewReport({jobDescription , setDescription , resumeFile})
+            setReport(data.newReport)
+
+        } catch (err) {
+            console.log(err)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const handleGetAllReports = async () => {
+        try {
+            setLoading(true)
+            const data = await getAllInterviewReports()
+            setReports(data.reports)
+        } catch (err) {
+            console.log(err)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+
+    const handleGetReportById = async (id) => {
+        try {
+            setLoading(true)
+            const data = await getInterviewReport(id)
+            setReport(data.report)
+        } catch (err) {
+            console.log(err)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+
+       const handleDeleteReport = async (id) => {
+        try {
+            setLoading(true)
+            const data = await deleteInterviewReport(id)
+            console.log(data.message)
+
+           setReports(reports.filter(report => report._id !== id))
+
+        } catch (err) {
+            console.log(err)
+        } finally {
+            setLoading(false)
+        }
+       }  
+
+
+
+    return{ 
+        loading ,
+        report ,
+        reports ,
+        handleGenerateReport,
+        handleGetAllReports,
+        handleGetReportById,
+        handleDeleteReport
+        
+    
+}
+}
